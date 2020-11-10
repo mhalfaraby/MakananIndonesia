3d0c123807791d63404699a09ffc3a45ade2d5be
mhalfaraby <mhalfaraby@gmail.com>
2020-11-10 18:15:49 +0700

    fix image

--- Makanan Indonesia/Controller/ViewController.swift
+++ Makanan Indonesia/Controller/ViewController.swift
@@ -16,22 +16,21 @@ class ViewController: UIViewController  {
         foodTableView.delegate = self
         navigationController?.navigationBar.prefersLargeTitles = true
         foodTableView.register(UINib(nibName: "FoodTableView", bundle: nil), forCellReuseIdentifier: "FoodCell")
-        print("test git")
     }
     
 }
 extension ViewController: UITableViewDataSource {
-    
+    // jumlah table berdasarkan array struct
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return foods.count
     }
+    // menampilkan cell nya sesuai array struct
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "FoodCell", for: indexPath) as? FoodTableView {
             let food = foods[indexPath.row]
             cell.nameFood.text = food.name
             cell.descriptionFood.text = food.description
             cell.photoFood.image = food.photo
-            
             return cell
         } else {
             return UITableViewCell()
@@ -39,6 +38,7 @@ extension ViewController: UITableViewDataSource {
     }
     
 }
+// user memilih table sesuai array dan berpindah ke detail view
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detail = DetailViewController(nibName: "DetailViewController", bundle: nil)
